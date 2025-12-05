6fabae9cdd69d2d898a51dcd5909557a0ae7f33d
koinu93 <eliza.starkey@yahoo.com>
2025-12-05 12:42:00 -0500

    Added text reading function

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        return f.read()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+    
+main()
