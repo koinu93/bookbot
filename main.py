266219117997f17a9a0b0d9c02c267a3e24cb144
koinu93 <eliza.starkey@yahoo.com>
2025-12-05 15:17:33 -0500

    Added character counting

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def main():
-    from stats import get_book_text, get_word_count
+    from stats import get_book_text, get_word_count, character_repeat_count
 
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    #print(text)
     word_total = get_word_count(book_path)
     print(f"Found {word_total} total words")
+    character_count = character_repeat_count(book_path)
+    print(character_count)
     
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,15 @@ def get_word_count(path_to_file):
         words = content.split()
         word_total = len(words)
     return word_total
+
+def character_repeat_count(path_to_file):
+    character_counts = {}
+    with open(path_to_file) as f:
+        content = f.read()
+        lower_case = content.lower()
+        for char in lower_case:
+            if char in character_counts:
+                character_counts[char] += 1
+            else:
+                character_counts[char] = 1
+    return character_counts
