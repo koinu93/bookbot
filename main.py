4c7c9793c1c34c00518a92e8d23d92037dec34dc
koinu93 <eliza.starkey@yahoo.com>
2025-12-07 12:29:53 -0500

    Added Formatted Report

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-def main():
-    from stats import get_book_text, get_word_count, character_repeat_count
+from stats import get_book_text, get_word_count, character_repeat_count, dictionary_pairs, sort_list
 
+def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     #print(text)
+    
     word_total = get_word_count(book_path)
-    print(f"Found {word_total} total words")
+    #print(f"Found {word_total} total words")
+    
     character_count = character_repeat_count(book_path)
-    print(character_count)
+    #print(character_count)
+
+    pairs = dictionary_pairs(book_path)
+
+    sort = sort_list(book_path)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_total} total words")
+    print("--------- Character Count -------")
+    for value in sort:
+        print(f"{value["char"]}: {value["num"]}")
+
     
 main()
--- stats.py
+++ stats.py
@@ -3,20 +3,35 @@ def get_book_text(path_to_file):
         return f.read()
     
 def get_word_count(path_to_file):
-    with open(path_to_file) as f:
-        content = f.read()
-        words = content.split()
-        word_total = len(words)
+    content = get_book_text(path_to_file)
+    words = content.split()
+    word_total = len(words)
     return word_total
 
 def character_repeat_count(path_to_file):
     character_counts = {}
-    with open(path_to_file) as f:
-        content = f.read()
-        lower_case = content.lower()
-        for char in lower_case:
-            if char in character_counts:
-                character_counts[char] += 1
-            else:
-                character_counts[char] = 1
+    content = get_book_text(path_to_file)
+    lower_case = content.lower()
+    for char in lower_case:
+        if char in character_counts:
+            character_counts[char] += 1
+        else:
+            character_counts[char] = 1
     return character_counts
+
+def dictionary_pairs(path_to_file):
+    character_counts = character_repeat_count(path_to_file)
+    pairs = []
+    for char, num in character_counts.items():
+        {"char": char, "num": num},
+        if char.isalpha() == True:
+            pairs.append({"char": char, "num": num})
+    return pairs
+
+def sort_on(items):
+    return items['num']
+
+def sort_list(path_to_file):
+    word_count = dictionary_pairs(path_to_file)
+    word_count.sort(reverse=True, key=sort_on)
+    return word_count
